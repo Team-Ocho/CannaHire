78240ca5a11d44a3faa51abfb69f6302ff4addf6
LaurenSantos23 <Lauren.Santosuosso@yahoo.com>
2019-01-05 13:01:51 -0500

    changed schema to whats in heroku

--- schema.sql
+++ schema.sql
@@ -1,23 +1,36 @@
-DROP DATABASE IF EXISTS canna_hire;
-CREATE DATABASE canna_hire;
+USE `hkbvtsjz9cohxt94`;
 
-USE canna_hire;
-
-CREATE TABLE users  (
+CREATE TABLE users (
     user_id INT AUTO_INCREMENT NOT NULL,
 	username VARCHAR(200) NOT NULL,
     first_name VARCHAR(200) NOT NULL,
     last_name VARCHAR(200) NOT NULL,
     company_name varchar(200) NOT NULL,
     position VARCHAR(200) NOT NULL,
+    zip_code INT NOT NULL,
     email VARCHAR(200) NOT NULL,
+    resume VARCHAR(200) NOT NULL,
+    cover_letter VARCHAR(200) NOT NULL,
     password VARCHAR(200) NOT NULL,
     PRIMARY KEY (user_id)
+);
+
+CREATE TABLE company_login (
+	company_id INT AUTO_INCREMENT NOT NULL,
+    company_name varchar(200) NOT NULL,
+    company_zip_code INT NOT NULL,
+    company_website varchar (200) NOT NULL,
+    company_email varchar(200) NOT NULL,
+    password VARCHAR(200) NOT NULL,
+    primary key (company_id)
+
 );
 
 CREATE TABLE job_postings (
     job_posting_id INT AUTO_INCREMENT NOT NULL,
 	job_title varchar(200) NOT NULL,
+    company_name varchar(200) NOT NULL,
+    city_state varchar(200) NOT NULL,
     salary varchar(200) NOT NULL,
     job_description varchar(600) NOT NULL,
     employee_requirements varchar(600) NOT NULL,
