734ffcc10da15f26ffd70bfd7d0074a74ea01758
Alexandra <Lexxiecole@gmail.com>
2019-01-12 12:53:17 -0500

    articles

--- src/components/pages/Learn.js
+++ src/components/pages/Learn.js
@@ -1,93 +1,92 @@
 import React from 'react';
 import "../../styles/Learn.css";
 import Sidebar from "../Sidebar";
+import API from '../../utils/API'
 
+class Learn extends React.Component {
+    state = {
+        articles: []
+    }
 
-const Learn = () => (
-    <div>
-        <Sidebar />
-        <div class="container-articles">
-            <div class="row">
-                <h1 className="header">Article Thumbnails</h1>
-            </div>
-            <div class="row">
-                <div class="col-md-5 col-lg-5">
-                    <div class="featured-article">
-                        <a href="#">
-                            <img src="http://placehold.it/482x350" alt="" class="thumb"></img>
-                        </a>
-                        <div class="block-title">
-                            <h2>Lorem ipsum dolor asit amet</h2>
-                            <p class="by-author"><small>By Jhon Doe</small></p>
+    componentDidMount() {
+        const blackList = ['engadget', 'mashable', 'bbc-news', 'the-guardian-au']
+        API().then(d => {
+            d.json().then(({ articles }) => {
+                console.log(articles)
+                this.setState({ articles: articles.filter(({ source }) => {
+                    return !blackList.includes(source.id)
+                }) })
+            })
+        })
+    }
+
+    render() {
+        return (<div>
+            <Sidebar />
+            <div className="container-articles">
+                <div className="row">
+                    <h1 className="header">Article Thumbnails</h1>
+                </div>
+                <div className="row">
+                    <div className="col-md-5 col-lg-5">
+                        <div className="featured-article">
+                            <a href="#">
+                                <img src="http://placehold.it/482x350" alt="" className="thumb"></img>
+                            </a>
+                            <div className="block-title">
+                                <h2>Lorem ipsum dolor asit amet</h2>
+                                <p className="by-author"><small>By Jhon Doe</small></p>
+                            </div>
                         </div>
                     </div>
+                    <div className="col-md-7 col-lg-7">
+                        <ul className="media-list main-list">
+                            <li className="media">
+                                <a className="pull-left" href="#">
+                                    <img className="media-object" src="http://placehold.it/150x90" alt="..."></img>
+                                </a>
+                                <div className="media-body">
+                                    <h4 className="media-heading">Lorem ipsum dolor asit amet</h4>
+                                    <p className="by-author">By Jhon Doe</p>
+                                </div>
+                            </li>
+                            <li className="media">
+                                <a className="pull-left" href="#">
+                                    <img className="media-object" src="http://placehold.it/150x90" alt="..."></img>
+                                </a>
+                                <div className="media-body">
+                                    <h4 className="media-heading">Lorem ipsum dolor asit amet</h4>
+                                    <p className="by-author">By Jhon Doe</p>
+                                </div>
+                            </li>
+                            <li className="media">
+                                <a className="pull-left" href="#">
+                                    <img className="media-object" src="http://placehold.it/150x90" alt="..."></img>
+                                </a>
+                                <div className="media-body">
+                                    <h4 className="media-heading">Lorem ipsum dolor asit amet</h4>
+                                    <p className="by-author">By Jhon Doe</p>
+                                </div>
+                            </li>
+                        </ul>
+                    </div>
                 </div>
-                <div class="col-md-7 col-lg-7">
-                    <ul class="media-list main-list">
-                        <li class="media">
-                            <a class="pull-left" href="#">
-                                <img class="media-object" src="http://placehold.it/150x90" alt="..."></img>
-                            </a>
-                            <div class="media-body">
-                                <h4 class="media-heading">Lorem ipsum dolor asit amet</h4>
-                                <p class="by-author">By Jhon Doe</p>
-                            </div>
-                        </li>
-                        <li class="media">
-                            <a class="pull-left" href="#">
-                                <img class="media-object" src="http://placehold.it/150x90" alt="..."></img>
-                            </a>
-                            <div class="media-body">
-                                <h4 class="media-heading">Lorem ipsum dolor asit amet</h4>
-                                <p class="by-author">By Jhon Doe</p>
-                            </div>
-                        </li>
-                        <li class="media">
-                            <a class="pull-left" href="#">
-                                <img class="media-object" src="http://placehold.it/150x90" alt="..."></img>
-                            </a>
-                            <div class="media-body">
-                                <h4 class="media-heading">Lorem ipsum dolor asit amet</h4>
-                                <p class="by-author">By Jhon Doe</p>
-                            </div>
-                        </li>
-                    </ul>
-                </div>
+            </div>
+        <div className="row">
+            <div className="leftcolumn">
+                {this.state.articles.map(({ author, title, description, publishedAt, url, }) => {
+                    return (<div className="card" key={url}>
+                    <h2>{title}</h2>
+                    {/* <img alt="#" src={this.urlToImage}> {urlToImage}</img> */}
+                    <h5>{author}, {publishedAt}</h5>
+    
+                    <p className="align-right">{description}</p>
+                </div>)
+                })}
             </div>
         </div>
-    <div class="row">
-        <div class="leftcolumn">
-            <div class="card">
-                <h2>TITLE HEADING</h2>
-                <h5>Title description, Dec 7, 2017</h5>
-
-                <p className="align-right">Sunt in culpa qui officia deserunt mollit anim id est laborum consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco.</p>
-            </div>
-            <div class="card">
-                <h2>TITLE HEADING</h2>
-                <h5>Title description, Sep 2, 2017</h5>
-                <p>Sunt in culpa qui officia deserunt mollit anim id est laborum consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco.</p>
-            </div>
-            <div class="card">
-                <h2>TITLE HEADING</h2>
-                <h5>Title description, Sep 2, 2017</h5>
-                <p>Sunt in culpa qui officia deserunt mollit anim id est laborum consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco.</p>
-            </div>
-            <div class="card">
-                <h2>TITLE HEADING</h2>
-                <h5>Title description, Sep 2, 2017</h5>
-                <p>Sunt in culpa qui officia deserunt mollit anim id est laborum consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco.</p>
-            </div>
-            </div>
-        </div>
-    </div>
-  
-
-
-
-
-  
-
-        )
+    </div>)
+    }
+}
 
 export default Learn
