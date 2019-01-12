21ee356af14036031fe7e851e34d433f4193bdbb
lblueter <logan11023@yahoo.com>
2019-01-12 14:35:53 -0500

    fix my mistake

--- server.js
+++ server.js
@@ -23,21 +23,21 @@ var PORT = process.env.PORT || 8080;
 var connection;
 
 if (process.env.JAWSDB_URL) {
-   connection = mysql.createConnection(process.env.JAWSDB_URL);
-  } else {
-    connect = mysql.createConnection({
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  connection = mysql.createConnection({
     host: process.env.db_host,
     // Your port; if not 3306
     port: process.env.db_port,
     // Your username
-    user:  process.env.db_user,
+    user: process.env.db_user,
     // Your password
-    password:  process.env.db_password,
+    password: process.env.db_password,
     database: "cannahire"
   });
 };
 
-connection.connect(function(err) {
+connection.connect(function (err) {
   if (err) throw err;
   console.log("connected as id " + connection.threadId)
 });
